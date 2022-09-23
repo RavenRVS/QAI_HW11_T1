aca6c2c24f51350e75502fc6672a35886a0d6bf3
Vitaly Remnev <remnevvs@mail.ru>
2022-09-23 10:18:47 +0300

    Added Repository and mockito

--- src/main/java/ru/netology/qai/hw11/javaqamaven/services/AfficheManager.java
+++ src/main/java/ru/netology/qai/hw11/javaqamaven/services/AfficheManager.java
@@ -2,28 +2,30 @@ package ru.netology.qai.hw11.javaqamaven.services;
 
 public class AfficheManager {
 
+    private AfficheRepository repo;
     private int countLastMovies = 10;
 
     public AfficheManager(){}
-    public AfficheManager(int LastMovies) {
-        countLastMovies = LastMovies;
+    public AfficheManager(AfficheRepository repo) {
+        this.repo = repo;
     }
-    private MovieItem[] movies = new MovieItem[0];
+
+    public AfficheManager(AfficheRepository repo, int LastMovies) {
+        this.countLastMovies = LastMovies;
+        this.repo = repo;
+    }
 
     public void add(MovieItem movie) {
-        MovieItem[] tmp = new MovieItem[movies.length + 1];
-        for (int i = 0; i < movies.length; i++) {
-            tmp[i] = movies[i];
-        }
-        tmp[tmp.length - 1] = movie;
-        movies = tmp;
+        repo.save(movie);
     }
 
     public MovieItem[] findAll() {
-        return movies;
+        MovieItem[] all = repo.findAll();
+        return all;
     }
 
     public MovieItem[] findLast() {
+        MovieItem[] movies = repo.findAll();
         int resultLength;
         if (countLastMovies < movies.length) {
             resultLength = countLastMovies;
--- src/test/java/ru/netology/qai/hw11/javaqamaven/services/AfficheManagerTest.java
+++ src/test/java/ru/netology/qai/hw11/javaqamaven/services/AfficheManagerTest.java
@@ -1,51 +1,52 @@
 package ru.netology.qai.hw11.javaqamaven.services;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
 
 public class AfficheManagerTest {
+
+    AfficheRepository repo = Mockito.mock(AfficheRepository.class);
+    AfficheManager mrepo = new AfficheManager(repo);
+    AfficheManager mrepo2 = new AfficheManager(repo,2);
+
 
     MovieItem movie1 = new MovieItem(1, "Бладшот");
     MovieItem movie2 = new MovieItem(2, "Вперед");
     MovieItem movie3 = new MovieItem(3, "Джентельмены");
 
+    @BeforeEach
+    public void setup() {
+        MovieItem[] movies = { movie1, movie2, movie3 };
+        doReturn(movies).when(repo).findAll();
+    }
+
     @Test
     public void add() {
-        AfficheManager manager = new AfficheManager();
-        manager.add(movie1);
-        manager.add(movie2);
-        manager.add(movie3);
 
         MovieItem[] expected = {movie1, movie2, movie3};
-        MovieItem[] actual = manager.findAll();
+        MovieItem[] actual = mrepo.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void findLastNotDefault() {
-        AfficheManager manager = new AfficheManager(2);
-
-        manager.add(movie1);
-        manager.add(movie2);
-        manager.add(movie3);
 
         MovieItem[] expected = {movie3, movie2};
-        MovieItem[] actual = manager.findLast();
+        MovieItem[] actual = mrepo2.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void findLastDefault() {
-        AfficheManager manager = new AfficheManager();
-
-        manager.add(movie1);
-        manager.add(movie2);
-        manager.add(movie3);
 
         MovieItem[] expected = {movie3, movie2, movie1};
-        MovieItem[] actual = manager.findLast();
+        MovieItem[] actual = mrepo.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
